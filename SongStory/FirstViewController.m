e58957e76b585cc320281d063059312dcf46733e
liuzenan <liuzenan711@gmail.com>
2013-03-09 15:34:27 +0800

    add

---
+++ SongStory/FirstViewController.m
@@ -0,0 +1,29 @@
+//
+//  FirstViewController.m
+//  SongStory
+//
+//  Created by Zenan on 9/3/13.
+//  Copyright (c) 2013 nus.cs3217. All rights reserved.
+//
+
+#import "FirstViewController.h"
+
+@interface FirstViewController ()
+
+@end
+
+@implementation FirstViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
