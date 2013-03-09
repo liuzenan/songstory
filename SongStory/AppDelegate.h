d4591c11e51380dcee8ce83dd02c4a3d9781e8c1
liuzenan <liuzenan711@gmail.com>
2013-03-09 13:05:58 +0800

    Initial Commit

---
+++ SongStory/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  SongStory
+//
+//  Created by Zenan on 9/3/13.
+//  Copyright (c) 2013 nus.cs3217. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
