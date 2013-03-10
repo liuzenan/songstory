d3538362deec98c7b79e4c7b51a0b84ab4700070
liuzenan <liuzenan711@gmail.com>
2013-03-10 14:45:18 +0800

    add

---
+++ SongStory/SongStoryView.h
@@ -0,0 +1,13 @@
+//
+//  SongStoryView.h
+//  SongStory
+//
+//  Created by Zenan on 10/3/13.
+//  Copyright (c) 2013 nus.cs3217. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SongStoryView : UIView
+
+@end
